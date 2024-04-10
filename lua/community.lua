e024ae4535ad8b3a306c61bfa48ce4677618e9ce
Gursharan Singh <gursharansingh4312@gmail.com>
2024-04-10 11:15:39 -0700

    initial config

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -8,5 +6,12 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.git.git-blame-nvim" },
+  { import = "astrocommunity.git.diffview-nvim" },
+  { import = "astrocommunity.scrolling.vim-smoothie" },
+  { import = "astrocommunity.bars-and-lines.heirline-vscode-winbar" },
+  { import = "astrocommunity.indent.indent-tools-nvim" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   -- import/override with your plugins folder
 }
--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -47,8 +45,8 @@ return {
         -- second key is the lefthand side of the map
 
         -- navigate buffer tabs with `H` and `L`
-        L = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-        H = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+        ["<Tab>"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
+        ["<S-Tab>"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
 
         -- mappings seen under group name "Buffer"
         ["<Leader>bD"] = {
@@ -63,11 +61,28 @@ return {
         -- this is useful for naming menus
         ["<Leader>b"] = { desc = "Buffers" },
         -- quick save
-        -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+        -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same commanda
+        ["<S-Up>"] = { "<cmd>resize -2<CR>", desc = "Resize split up" },
+        ["<S-Down>"] = { "<cmd>resize +2<CR>", desc = "Resize split down" },
+        ["<S-Left>"] = { "<cmd>vertical resize -2<CR>", desc = "Resize split left" },
+        ["<S-Right>"] = { "<cmd>vertical resize +2<CR>", desc = "Resize split right" },
+
+        ["<A-j>"] = { "<cmd>:m +1<CR>", desc = "move line down" },
+        ["<A-k>"] = { "<cmd>:m -2<CR>", desc = "move line up" },
+
+        ["<leader>W"] = { "<cmd>:noa w<CR>", desc = "save without formatting" },
+
+        ["<S-e>"] = { "<cmd>:lua vim.diagnostic.open_float()<CR>", desc = "show error in float" },
+
+        ["<Leader>gc"] = { "<cmd>:GitBlameOpenCommitURL<CR>", desc = "show error in float" },
       },
       t = {
         -- setting a mapping to false will disable it
         -- ["<esc>"] = false,
+      },
+      v = {
+        ["<A-j>"] = { "<cmd>:m +1<CR>", desc = "move line down" },
+        ["<A-k>"] = { "<cmd>:m -2<CR>", desc = "move line up" },
       },
     },
   },
